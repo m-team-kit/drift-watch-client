5da2278cb0ebc1e73710864d3a912424eacd6957
Borja Esteban <boressan@outlook.com>
2024-12-20 11:03:13 +0100

    test: update tests for find_experiment

--- tests/experiments/test_find.py
+++ tests/experiments/test_find.py
@@ -10,27 +10,28 @@ import drift_monitor
 
 
 @pytest.fixture(scope="function", autouse=True)
-def mocks(request_mock, experiment):
+def mocks(request_mock, db_experiment):
     """Mock the requests module."""
     request_mock.post.return_value = mock.MagicMock(
-        json=lambda: [experiment],
+        json=lambda: [db_experiment],
         headers={"X-Pagination": "somevalues"},
     )
 
 
 @pytest.fixture(scope="function")
-def response(mocks, experiment_name):
+def experiment(mocks, experiment_name):
     """Create a drift run on the drift monitor server."""
     return drift_monitor.find_experiment(experiment_name)
 
 
 @pytest.mark.parametrize("experiment_name", ["experiment_1"])
-def test_request(request_mock, response):
+@pytest.mark.usefixtures("experiment")
+def test_request(request_mock):
     """Test the drift run was created on the server."""
     assert request_mock.post.call_count == 1
 
 
 @pytest.mark.parametrize("experiment_name", ["experiment_1"])
-def test_returns(response, experiment):
+def test_returns(experiment, db_experiment):
     """Test correct return values from new experiment"""
-    assert response == experiment
+    assert experiment.__dict__ == db_experiment
